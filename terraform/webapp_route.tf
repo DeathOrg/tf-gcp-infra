dfaea5fe9ad9877d29f735fb4584733e110c3e78
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-02-15 15:54:15 -0500

    add subnet name to routes names

--- terraform/webapp_route.tf
+++ terraform/webapp_route.tf
@@ -1,8 +1,7 @@
 # Create routes
 resource "google_compute_route" "webapp_route" {
   count             = length(var.vpc_names)
-#   name              = "${var.vpc_names[count.index]}-${var.webapp_route_name}"
-  name              = var.webapp_route_name
+  name              = "${var.vpc_names[count.index]}-${var.webapp_route_name}"
   network           = google_compute_network.my_vpc[count.index].self_link
   dest_range        = var.webapp_route_dest_range
   next_hop_gateway  = "default-internet-gateway"
