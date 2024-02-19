d3f9e26c699bc181ca8e06e8432e8c19d1627a8b
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-02-19 11:15:42 -0500

    revert the changes

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -2,7 +2,7 @@
 resource "google_compute_subnetwork" "db_subnet" {
   count         = length(var.vpc_names)
 #   name          = "${var.vpc_names[count.index]}-${var.db_subnet_name}"
-  name          = var.db_subnet
+  name          = var.db_subnet_name
   ip_cidr_range = var.db_subnet_cidr_range
   network       = google_compute_network.my_vpc[count.index].self_link
   region        = var.subnet_region
