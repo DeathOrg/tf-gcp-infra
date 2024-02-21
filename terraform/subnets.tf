4fa8002cb34187ce3f5d807117296e64d66a3ccb
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-02-21 18:48:35 -0500

    add firewall and compute engine

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,7 +1,6 @@
 # Create subnets for each VPC
 resource "google_compute_subnetwork" "db_subnet" {
   count         = length(var.vpc_names)
-#   name          = "${var.vpc_names[count.index]}-${var.db_subnet_name}"
   name          = var.db_subnet_name
   ip_cidr_range = var.db_subnet_cidr_range
   network       = google_compute_network.my_vpc[count.index].self_link
@@ -10,7 +9,6 @@ resource "google_compute_subnetwork" "db_subnet" {
 
 resource "google_compute_subnetwork" "webapp_subnet" {
   count         = length(var.vpc_names)
-#   name          = "${var.vpc_names[count.index]}-${var.webapp_subnet_name}"
   name          = var.webapp_subnet_name
   ip_cidr_range = var.webapp_subnet_cidr_range
   network       = google_compute_network.my_vpc[count.index].self_link
