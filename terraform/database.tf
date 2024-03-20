47c517734d45697df8c0a4170babbdd0ccda517f
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-03-20 02:18:36 -0400

    use variable for everything

--- terraform/database.tf
+++ terraform/database.tf
@@ -15,7 +15,6 @@ resource "google_sql_database_instance" "webapp_db_instance" {
     ip_configuration {
       ipv4_enabled    = var.db_instance_ipv4_enabled
       private_network = google_compute_network.my_vpc.id
-      #   enable_private_path_for_google_cloud_services = var.db_instance_enable_private_path
     }
     availability_type = var.db_instance_availability_type
     backup_configuration {
--- terraform/dns_record.tf
+++ terraform/dns_record.tf
@@ -1,9 +1,9 @@
 resource "google_dns_record_set" "web_server" {
-  name = "sourabhk.com."
-  type = "A"
+  name = var.my_domain
+  type = var.dns_record_A
   ttl  = 300
 
-  managed_zone = "webapp-zone"
+  managed_zone = var.webserver_managed_zone
 
   rrdatas = [google_compute_instance.webapp_instance.network_interface[0].access_config[0].nat_ip]
 }
