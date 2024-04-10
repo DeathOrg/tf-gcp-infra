9386cbb1edd7effa7af425f2e98dd500bf5c0f85
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-04-10 06:54:56 -0400

    output some imp info

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,14 @@
-output "private_ip_address_cloudsql" {
+output "DATABASE_HOST" {
   value       = google_sql_database_instance.webapp_db_instance.private_ip_address
   description = "The private IP address of the newly created My SQL"
+}
+
+output "DATABASE_PASSWORD" {
+  value       = google_sql_user.db_user.password
+  sensitive = true
+  # terraform output -no-color DATABASE_PASSWORD
+}
+
+output "IT_KEY_RING" {
+  value       = google_kms_key_ring.app_keys.name
 }
--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -1,9 +1,9 @@
 # Create a Pub/Sub topic
 resource "google_pubsub_topic" "verify_email" {
   name = var.pubsub_topic_name
-#   message_storage_policy {
-#     allowed_persistence_regions = [var.region]
-#   }
+  message_storage_policy {
+    allowed_persistence_regions = [var.region]
+  }
 }
 
 # Create a Pub/Sub subscription
